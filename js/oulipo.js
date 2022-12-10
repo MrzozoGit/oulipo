50cc99d7fdc02feba3a44d2992d7859ef0ce8ce8
Mrzozo (Enzo Bassot) <enzo.bassot@gmail.com>
2022-12-10 17:49:30 +0100

    suite code pc portable + curseur

--- js/oulipo.js
+++ js/oulipo.js
@@ -1,3 +1,32 @@
+// REPLACER DANS LE MVC //
+
+const cursor = document.querySelector('.cursor');
+let mouse = { x: -200, y: -200 };
+let pos = { x: 0, y: 0 };
+const speed = 1; // between 0 and 1
+
+const updatePosition = () => {
+  pos.x += (mouse.x - pos.x) * speed - 25/2;
+  pos.y += (mouse.y - pos.y) * speed - 25/2;
+  cursor.style.transform = 'translate3d(' + pos.x + 'px ,' + pos.y + 'px, 0)';
+};
+
+const updateCoordinates = e => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+}
+
+window.addEventListener('mousemove', updateCoordinates);
+
+function loop() {
+  updatePosition();
+  requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
+
+//////////////////////////////////
+
 class Poeme {
     constructor(lines) {
         this.lines = lines;
@@ -71,21 +100,50 @@ var C = {
 
 var V = {
     init: function() {
+        V.cursor = document.querySelector('.cursor');
+        V.hoverable = document.querySelectorAll('.hoverable')
+
+        V.showHaikuButton = document.querySelector('#showHaiku');
         V.newHaikuButton = document.querySelector('#generateHaiku');
         V.haiku = document.querySelector('#haiku');
+        V.haikuDiv = document.querySelector('.view__haiku');
+
+        V.showSonnetButton = document.querySelector('#showSonnet');
         V.newSonnetButton = document.querySelector('#generateSonnet');
         V.sonnet = document.querySelector('#sonnet');
+        V.sonnetDiv = document.querySelector('.view__sonnet');
 
         V.bindEvents();
     },
 
     bindEvents: function() {
+        V.showHaikuButton.addEventListener("click", V.showHaiku);
+        V.showSonnetButton.addEventListener("click", V.showSonnet);
+
         V.newHaikuButton.addEventListener("click", C.newHaikuHandler);
         V.newSonnetButton.addEventListener("click", C.newSonnetHandler);
+
+        V.hoverable.forEach(e => {
+            e.addEventListener("mouseover", V.addHoverButton);
+            e.addEventListener("mouseout", V.removeHoverButton);
+        });
+    },
+
+    // optimiser pour retirer à toutes les combinatoires
+    showHaiku: function() {
+        V.resetHaiku();
+        V.haikuDiv.classList.add("active");
+        V.sonnetDiv.classList.remove("active");
+    },
+
+    showSonnet: function() {
+        V.resetSonnet();
+        V.sonnetDiv.classList.add("active");
+        V.haikuDiv.classList.remove("active");
     },
 
     updateHaiku: function(lines) {
-        V.haiku.innerHTML = "";
+        V.resetHaiku();
         lines.forEach(line => {
             var li = document.createElement("li");
             li.textContent = line;
@@ -94,12 +152,28 @@ var V = {
     },
 
     updateSonnet: function(lines) {
-        V.sonnet.innerHTML = "";
+        V.resetSonnet();
         lines.forEach(line => {
             var li = document.createElement("li");
             li.textContent = line;
             V.sonnet.append(li);
         })
+    },
+
+    addHoverButton: function() {
+        V.cursor.classList.add("cursor__hovering");
+    },
+
+    removeHoverButton: function() {
+        V.cursor.classList.remove("cursor__hovering");
+    },
+
+    resetHaiku: function() {
+        V.haiku.innerHTML = "";
+    },
+
+    resetSonnet: function() {
+        V.sonnet.innerHTML = "";
     }
 }
 
